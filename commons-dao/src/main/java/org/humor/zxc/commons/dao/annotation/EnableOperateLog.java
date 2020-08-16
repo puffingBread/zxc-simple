220109ccdff1d00778e68ec65918be42954aa19c
xuzz <xuzz@yoozoo.com>
2020-08-16 22:29:49 +0800

    test

---
+++ commons-dao/src/main/java/org/humor/zxc/commons/dao/annotation/EnableOperateLog.java
@@ -0,0 +1,12 @@
+package org.humor.zxc.commons.dao.annotation;
+
+/**
+ * TODO ADD DESCRIPTION
+ * Date: 2020/8/16
+ * Time: 10:18 下午
+ *
+ * @author xuzz
+ */
+public @interface EnableOperateLog {
+    
+}
